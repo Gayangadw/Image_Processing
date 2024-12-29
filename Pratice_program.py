96d63b4011d7ded0dd0c85a8cb150f3ed523d75b
Gayanga Dulanjana <158056564+Gayangadw@users.noreply.github.com>
2024-12-29 22:50:20 +0530

    Add files via upload

---
+++ Pratice_program.py
@@ -0,0 +1,53 @@
+# from numpy.ma.core import minimum, maximum
+#
+# number_1=int(input("Enter number 1 : " ))
+# number_2=int(input("Enter number 2 : " ))
+# number_3=int(input("Enter number 3 : " ))
+#
+# minimum=min(number_1,number_2,number_3)
+# maximum=max(number_1,number_3,number_2)
+# print("Maximum number is : ",maximum)
+# print("Minimum number is : ",minimum)
+#
+# length=float(input("Enter the length : "))
+# width=float(input("Enter the width : "))
+# # area=length*width
+# # perimeter=2*(length+width)
+# # print("The Area is : ",area)
+# # print("The perimeter is : ",perimeter)
+#
+# if length==width:
+#     print("This is the square ")
+# else:
+#     print("This is not a square ")
+# year=int(input("What is your birth year? "))
+# age=2024-int(year)
+# print("your age is :",age)
+#
+# if age >= 40:
+#     print("You are an adult ")
+# else:
+#     print("You are the young ")
+# x=99
+# while x>0:
+#     print(x)
+#     x=x-1
+# x=1
+# while x<= 10:
+#     number = int(input("Enter the number : "))
+#     x=x+1
+#     sum=10*(number)
+# print(sum)
+# def sum(no1,no2,no3):
+#     ans=no1+no2+no3
+#     return ans
+# print(sum(45,55,78))
+# def avg(no1,no2,no3,no4):
+#     ans=(no1+no2+no3+no4)/4
+#     return ans
+# print(avg(34,67,78,56))
+
+x=["apples","banana",45.7,89,True]
+i=0
+for i in range(len(x)):
+    print(x[i])
